f629c6a850a8679cc8c53acae452f8d45de929cd
Mohaideen Abuthahir <127249703+Abuthahir98@users.noreply.github.com>
2024-08-24 23:05:06 +0530

    Update Print.py

--- Print.py
+++ Print.py
@@ -1,11 +1,21 @@
-# Python program to check if the input number is odd or even.
-# A number is even if division by 2 gives a remainder of 0.
-# If the remainder is 1, it is an odd number.
+# Python program to check if year is a leap year or not
 
+year = 2000
 
-num = 4
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
 else:
-   print("{0} is Odd".format(num))
-Run Code
+    print("{0} is not a leap year".format(year))
